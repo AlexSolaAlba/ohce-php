a781fb6610d663751c34de700eafed3e36a71f11
Alejandro Sola <alejandrosolaalba@gmail.com>
2025-03-30 18:16:34 +0200

    [] - Creacion repo ohce

---
+++ src/Ohce.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Deg540\StringCalculatorPHP;
+
+use function PHPUnit\Framework\equalTo;
+
+class Ohce
+{
+    // TODO: Ohce Kata
+
+    /**
+     * @param float $hour
+     * @param string $name
+     *
+     * @return string
+     */
+    public function Greetings(float $hour,string $name): string
+    {
+        if ($hour >= 6 && $hour < 12) return "Buenos dias " . $name;
+        if ($hour >= 12 && $hour < 20) return "Buenas tardes " . $name;
+        return "Buenas noches " . $name;
+    }
+
+    /**
+     * @param string $word
+     *
+     * @return string
+     */
+    public function CheckPalindrome(string $word): string
+    {
+        if ($this->isPalindrome($word)) return "Bonita palabra";
+        return "";
+    }
+
+    /**
+     * @param string $word
+     *
+     * @return bool
+     */
+    public function isPalindrome(string $word): bool
+    {
+        $palindrome = true;
+        for ($i = 0; $i < strlen($word)/2; $i++) {
+            if ($word[$i] != $word[strlen($word) - $i-1]){
+                $palindrome = false;
+            }
+        }
+        return $palindrome;
+    }
+
+    /**
+    * @param string $word
+    *
+    * @return string
+    */
+    public function Stop(string $word): string
+    {
+        if($word == "Stop") return "Adios";
+        return "";
+    }
+}
---
+++ tests/OhceTest.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Deg540\StringCalculatorPHP\Test;
+
+use PHPUnit\Framework\TestCase;
+use Deg540\StringCalculatorPHP\Ohce;
+
+class OhceTest extends TestCase
+{   // TODO: String Calculator Kata Tests
+    private Ohce $ohce;
+
+    protected function setUp() : void
+    {
+        parent::setUp();
+        $this->ohce = new Ohce();
+    }
+
+
+    /**
+    * @test
+    */
+    public function given6AndNameReturnsBuenosDias() : void
+    {
+        $returnValue = $this->ohce->Greetings(6,"ale");
+        $this->assertEquals("Buenos dias ale",$returnValue);
+    }
+
+    /**
+     * @test
+     */
+    public function given12AndNameReturnsBuenosDias() : void
+    {
+        $returnValue = $this->ohce->Greetings(12,"ale");
+        $this->assertEquals("Buenas tardes ale",$returnValue);
+    }
+
+    /**
+     * @test
+     */
+    public function given20AndNameReturnsBuenosDias() : void
+    {
+        $returnValue = $this->ohce->Greetings(20,"ale");
+        $this->assertEquals("Buenas noches ale",$returnValue);
+    }
+
+    /**
+     * @test
+     */
+    public function givenANotPalindromeWordReturnsNothing() : void
+    {
+        $returnValue = $this->ohce->CheckPalindrome("ale");
+        $this->assertEquals("",$returnValue);
+    }
+
+    /**
+     * @test
+     */
+    public function givenAPalindromeWordReturnsBonitaPalabra() : void
+    {
+        $returnValue = $this->ohce->CheckPalindrome("ala");
+        $this->assertEquals("Bonita palabra",$returnValue);
+    }
+
+    /**
+     * @test
+     */
+    public function givenStopReturnsAdios() : void
+    {
+        $returnValue = $this->ohce->Stop("Stop");
+        $this->assertEquals("Adios",$returnValue);
+    }
+}
